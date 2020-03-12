b876d717cd89ec9873c5b7dbb54aec97fdb76116
Jeff Wong <v-jwong1@expedia.com>
2020-03-12 20:31:56 +1000

    call DriverManager.getConnection to connect to database

--- src/main/java/com/pluralsight/corejdbc/m2c1/Main.java
+++ src/main/java/com/pluralsight/corejdbc/m2c1/Main.java
@@ -1,20 +1,35 @@
 package com.pluralsight.corejdbc.m2c1;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		try {
-			ConnectComponent comp = new ConnectComponent();
+//		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC")){
+//
+//			return conn.isValid(2);
+//
+//		}
 
-			if (comp.tryConnection()) {
-				System.out.println("Demo m2c1:");
-				System.out.println("The attempt to Connection was a SUCCESS");
-			} else {
-				System.out.println("Demo m2c1:");
-				System.out.println("The attempt to Connection FAILED");
-			}
+		try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC")) {
+			System.out.println(connection.isValid(2));
+			System.out.println("The attempt to Connection was a SUCCESS");
 		} catch (Exception exception) {
 			util.ExceptionHandler.handleException(exception);
 		}
+
+//		try {
+//			ConnectComponent comp = new ConnectComponent();
+//
+//			if (comp.tryConnection()) {
+//				System.out.println("Demo m2c1:");
+//				System.out.println("The attempt to Connection was a SUCCESS");
+//			} else {
+//				System.out.println("Demo m2c1:");
+//				System.out.println("The attempt to Connection FAILED");
+//			}
+//		} catch (Exception exception) {
+//			util.ExceptionHandler.handleException(exception);
+//		}
 	}
 }
