1df082f97898d95b56856efd167f6a2b88163b07
Jeff Wong <v-jwong1@expedia.com>
2020-03-12 20:18:14 +1000

    remove white spaces

--- src/main/java/com/pluralsight/corejdbc/m2c1/Main.java
+++ src/main/java/com/pluralsight/corejdbc/m2c1/Main.java
@@ -2,9 +2,7 @@ package com.pluralsight.corejdbc.m2c1;
 
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
-
 		try {
 			ConnectComponent comp = new ConnectComponent();
 
@@ -19,5 +17,4 @@ public class Main {
 			util.ExceptionHandler.handleException(exception);
 		}
 	}
-
 }
