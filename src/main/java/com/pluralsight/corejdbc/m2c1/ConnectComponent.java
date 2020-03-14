c5d0f3d5847b06735ab741ff7f2bf01b1e8ee88f
Jeff Wong <v-jwong1@expedia.com>
2020-03-14 12:25:15 +1000

    connect to mySQL instance on aws

--- src/main/java/com/pluralsight/corejdbc/m2c1/ConnectComponent.java
+++ src/main/java/com/pluralsight/corejdbc/m2c1/ConnectComponent.java
@@ -8,8 +8,9 @@ public class ConnectComponent {
 	
 	public boolean tryConnection() throws Exception{	
 
-		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC")){
-				
+//		try (Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC")){
+		try (Connection conn = DriverManager.getConnection("jdbc:mysql://jeff-mysql-sydney.c74usuhv8zuq.ap-southeast-2.rds.amazonaws.com:3306/classicmodels?user=root&password=jeffpluralsight&serverTimezone=UTC")){
+
 			return conn.isValid(2);
     				   
 		}
