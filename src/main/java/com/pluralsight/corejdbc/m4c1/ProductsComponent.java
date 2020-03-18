ffedce35e3afa1a0aca541914a017da786fbe4d7
Jeff Wong <v-jwong1@expedia.com>
2020-03-18 15:15:02 +1000

    call connection.createStatement on statement object

--- src/main/java/com/pluralsight/corejdbc/m4c1/ProductsComponent.java
+++ src/main/java/com/pluralsight/corejdbc/m4c1/ProductsComponent.java
@@ -13,6 +13,6 @@ public class ProductsComponent {
 		Connection connection = DriverManager.getConnection(
 //				"jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC");
 				"jdbc:mysql://jeff-mysql-sydney.c74usuhv8zuq.ap-southeast-2.rds.amazonaws.com:3306/classicmodels?user=root&password=jeffpluralsight&serverTimezone=UTC");
-
+		Statement statement = connection.createStatement();
 	}
 }
