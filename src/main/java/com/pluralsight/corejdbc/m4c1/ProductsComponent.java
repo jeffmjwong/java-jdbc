8577e5abd26fc567e3a34b843fbaae11e151372a
Jeff Wong <v-jwong1@expedia.com>
2020-03-18 15:39:55 +1000

    call close method on statement

--- src/main/java/com/pluralsight/corejdbc/m4c1/ProductsComponent.java
+++ src/main/java/com/pluralsight/corejdbc/m4c1/ProductsComponent.java
@@ -11,13 +11,14 @@ public class ProductsComponent {
 	public void printProductList() throws Exception {
 
 		Connection connection = DriverManager.getConnection(
-//				"jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC");
-				"jdbc:mysql://jeff-mysql-sydney.c74usuhv8zuq.ap-southeast-2.rds.amazonaws.com:3306/classicmodels?user=root&password=jeffpluralsight&serverTimezone=UTC");
+				"jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC");
+//				"jdbc:mysql://jeff-mysql-sydney.c74usuhv8zuq.ap-southeast-2.rds.amazonaws.com:3306/classicmodels?user=root&password=jeffpluralsight&serverTimezone=UTC");
 		Statement statement = connection.createStatement();
 		ResultSet resultSet = statement.executeQuery("SELECT * FROM products");
 
 		System.out.println(resultSet);
 
 		resultSet.close();
+		statement.close();
 	}
 }
