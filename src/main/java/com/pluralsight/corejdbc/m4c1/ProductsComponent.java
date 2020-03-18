c5d06ffc9011b9b06dbf65c23c5284a495203f2d
Jeff Wong <v-jwong1@expedia.com>
2020-03-18 15:11:41 +1000

    write a sql query from scratch

--- src/main/java/com/pluralsight/corejdbc/m4c1/ProductsComponent.java
+++ src/main/java/com/pluralsight/corejdbc/m4c1/ProductsComponent.java
@@ -11,19 +11,8 @@ public class ProductsComponent {
 	public void printProductList() throws Exception {
 
 		Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC");
-		Statement statement = connection.createStatement();
-		ResultSet resultSet = 
-		statement.executeQuery("SELECT * FROM products");	
-		
-		while(resultSet.next()) {
-			String name = resultSet.getString("productName");
-			System.out.println(name);
-		}
+//				"jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC");
+				"jdbc:mysql://jeff-mysql-sydney.c74usuhv8zuq.ap-southeast-2.rds.amazonaws.com:3306/classicmodels?user=root&password=jeffpluralsight&serverTimezone=UTC");
 
-		resultSet.close();
-		statement.close();
-		connection.close();
-		
 	}
 }
