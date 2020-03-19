23f921ea189ea1969dd607b8fb0f46050c29dea3
Jeff Wong <v-jwong1@expedia.com>
2020-03-19 15:24:49 +1000

    get productName, quantityInStock and buyPrice in a single query

--- src/main/java/com/pluralsight/corejdbc/m4c5/Main.java
+++ src/main/java/com/pluralsight/corejdbc/m4c5/Main.java
@@ -3,7 +3,6 @@ package com.pluralsight.corejdbc.m4c5;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
-
 		try {
 			ProductsComponent comp = new ProductsComponent();
 			comp.printProductList();
