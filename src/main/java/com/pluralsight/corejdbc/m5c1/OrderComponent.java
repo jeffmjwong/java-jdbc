6c016fc3822e16b99b338f8abc23f4c966c20839
Jeff Wong <v-jwong1@expedia.com>
2020-03-21 14:16:28 +1000

    uppercase SQL query keywords

--- src/main/java/com/pluralsight/corejdbc/m5c1/OrderComponent.java
+++ src/main/java/com/pluralsight/corejdbc/m5c1/OrderComponent.java
@@ -6,7 +6,7 @@ import java.sql.PreparedStatement;
 
 public class OrderComponent {
 	public void updateOrderQuantity(int orderNumber, String productCode, int newQuantity) throws Exception {
-		final String query = "UPDATE orderdetails set quantityOrdered = ? where orderNumber = ? AND productCode = ?";
+		final String query = "UPDATE orderdetails SET quantityOrdered = ? WHERE orderNumber = ? AND productCode = ?";
 
 		try (
 				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC");
