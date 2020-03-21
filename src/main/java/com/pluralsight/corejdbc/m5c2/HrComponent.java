60c06886abd1171fe5a69ad683465b5d4e80d654
Jeff Wong <v-jwong1@expedia.com>
2020-03-21 16:46:10 +1000

    refactor code and remove redundant white spaces

--- src/main/java/com/pluralsight/corejdbc/m5c2/HrComponent.java
+++ src/main/java/com/pluralsight/corejdbc/m5c2/HrComponent.java
@@ -4,26 +4,18 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 
-
 public class HrComponent {
 	
-	public int replaceSalesManager(String managerBeingReplaced, String replacementManager) throws Exception{
-		
-		try(Connection connection = DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC");
-		
-		PreparedStatement preparedStatement = 
-				  connection.prepareStatement(
-						  "UPDATE employees SET reportsTo = ? WHERE reportsTo = ?");){
-				
-		preparedStatement.setString(1, replacementManager);
-		preparedStatement.setString(2, managerBeingReplaced);
-		int count = preparedStatement.executeUpdate();
+	public int replaceSalesManager(String managerBeingReplaced, String replacementManager) throws Exception {
+		final String query = "UPDATE employees SET reportsTo = ? WHERE reportsTo = ?";
+		try (
+				Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/classicmodels?user=root&password=pluralsight&serverTimezone=UTC");
+				PreparedStatement preparedStatement = connection.prepareStatement(query)
+		) {
+			preparedStatement.setString(1, replacementManager);
+			preparedStatement.setString(2, managerBeingReplaced);
 
-		return count;
-		
+			return preparedStatement.executeUpdate();
 		}
-		
 	}
-
 }
